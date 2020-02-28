bd20ea243d3ad5f0abe390e50eed449455d5434f
Oliver Davies <oliver@oliverdavies.uk>
2020-02-28 18:49:33 +0000

    Add default site files

---
+++ web/sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+$databases['default']['default'] = [
+  'driver' => $_SERVER['DATABASE_DRIVER'],
+  'host' => $_SERVER['DATABASE_HOST'],
+  'database' => $_SERVER['DATABASE_NAME'],
+  'username' => $_SERVER['DATABASE_USER'],
+  'password' => $_SERVER['DATABASE_PASSWORD'],
+  'port' => $_SERVER['DATABASE_PORT'],
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'collation' => 'utf8mb4_general_ci',
+];
+
+$settings["config_sync_directory"] = '../config/default';
